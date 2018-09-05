2807650a21460e86fc1a6c4ef1b160cf40e74cbc
wajahatAnwar <wajahat@ikarobar.com>
2018-09-06 00:04:23 +0500

    Frist ever commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Stack-Apps
+                    Welcome to RobeSale App
                 </div>
             </div>
         </div>
