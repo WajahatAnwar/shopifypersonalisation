055df821cc026f802c74543ea589656fe206fa7c
wajahatAnwar <wajahat@ikarobar.com>
2018-09-09 14:44:21 +0500

    Making Changes according to the selected variants

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -1280,53 +1280,7 @@ class ShopifyController extends Controller
 			 </div>
 		  </div>
 	   </div>
-	</div>
-	<script>
-	$(document).ready(function(){
-		var dating= document.getElementById('timeanddate').value;
-        if(dating != '')
-		{
-			var countDownDate = new Date(dating).getTime();
-		}else
-		{
-			var dating2= document.getElementById('timer2').value;
-			var countDownDate = new Date(dating2).getTime();
-		}
-		// Set the date we're counting down to
-		
-	
-		// Update the count down every 1 second
-		var x = setInterval(function() {
-	
-		// Get todays date and time
-		var now = new Date().getTime();
-		
-		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
-		
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		
-		// Output the result in an element with id='demo'
-		document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h '
-		+ minutes + 'm ' + seconds + 's ';
-		document.getElementById('demo1').innerHTML = days + 'd ' + hours + 'h '
-		+ minutes + 'm ' + seconds + 's ';
-		
-		// If the count down is over, write some text 
-		if (distance < 0) {
-			clearInterval(x);
-			document.getElementById('demo').innerHTML = 'EXPIRED';
-			document.getElementById('demo1').innerHTML = 'EXPIRED';
-		}
-		}, 1000);
-      
-      $('#demo1').insertBefore ('.btn--wide:contains(Add to Cart)'); 
-	});
-	</script>";
+	</div>";
         $postData = [
                             "key" => "snippets/personalisation-popup.liquid",
                             "value" => $view
