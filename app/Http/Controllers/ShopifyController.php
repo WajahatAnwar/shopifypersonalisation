940a30ef52ce9683184088a537b472ddad94e0e7
wajahatAnwar <wajahat@ikarobar.com>
2018-09-12 23:02:38 +0500

    Adding option for the disabling key from the front end

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -188,6 +188,12 @@ class ShopifyController extends Controller
 			->where('meta_field_id', $meta_id)->delete();
 		}
 		
+		$shop = Shop::where('myshopify_domain' , $shopUrl)->first();
+		$shopProducts = $this->shopify->setShopUrl($shop->myshopify_domain)
+					->setAccessToken($shop->access_token)
+					->get('admin/products.json',[ 'limit' => 250 , 'page' => 1 ]);
+		$product_disable_key = DB::Table('product_disable_key')->where('shopify_store_id', $shopify_id )->get();
+		return view('home.index' , ['shop' => $shop , 'settings' => $shop->settings, "shop_products" => $shopProducts, "product_disable_key" => $product_disable_key,'success' => '1']);
 
 	}
 
