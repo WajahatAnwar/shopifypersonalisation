a4b9e172417b6891a05b5e8a0a2b40056b9acaa7
wajahatAnwar <wajahat@ikarobar.com>
2018-10-01 10:32:42 +0500

    A Bug

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -35,7 +35,7 @@ class ShopifyController extends Controller
 				$shopProducts = $this->shopify->setShopUrl($shop->myshopify_domain)
 				->setAccessToken($shop->access_token)
 				->get('admin/products.json',[ 'limit' => 250 , 'page' => 1 ]);
-				// dd($shopProducts);
+				dd($shopProducts);
 				$product_disable_key = DB::Table('product_disable_key')->where('shopify_store_id', session('shopifyId') )->get();
 				return view('home.index' , ['shop' => $shop , 'settings' => $shop->settings, "shop_products" => $shopProducts, "product_disable_key" => $product_disable_key, 'success' => '2']);
 
